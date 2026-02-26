a49ffc446f1a9e721f32260e65ac1b04868d4f0f
Muhammaddiyor2010 <kuchkarovmuhammaddiyor8@gmail.com>
2026-02-26 13:36:46 +0300

    sqlite

--- core/db.py
+++ core/db.py
@@ -3,10 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost/for_pro" 
+SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
 
-engine =  create_engine(
-    SQLALCHEMY_DATABASE_URL
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}  # Faqat SQLite uchun
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
